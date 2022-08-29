ba19a107bc48b2c6c889b04090115f90b712e9a4
Aluno de Front End Senai <AlunoDeFrontEndSenai@gmail.com>
2022-08-29 17:25:55 -0300

    Primeira Parte Angular

---
+++ src/assets/js/script.js
@@ -0,0 +1,22 @@
+// jQuery
+$(document).ready(function () {
+    // Todas as funções do jQuery
+
+    $(".btn-buy").click(function(){
+        alert("Este produto não está disponível");
+    });
+
+    
+    
+});
+
+
+
+
+function abreFechaMenu() {
+    // navbar -tag nav
+    let navbar = document.querySelector(".menu");
+    navbar.classList.toggle("mostraEscondeMenu");
+    // console.log(navbar);
+}
+
